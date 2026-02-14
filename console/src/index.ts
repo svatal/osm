360d2e77b2b12074958336043bbb375be5d519d0
svatal <swata@centrum.cz>
2026-02-14 20:30:10 +0100

    prepare for another osm project

--- console/src/index.ts
+++ console/src/index.ts
@@ -4,10 +4,7 @@ import { exportStatsToFile } from "./stats.js";
 import * as fs from "fs";
 import { Data } from "./collect.js";
 import { exportMapToFile } from "./map.js";
-import {
-  exportRelationAttributeMapsToFiles,
-  exportWayAttributeMapsToFiles,
-} from "./attributeMaps.js";
+import { exportRelationAttributeMapsToFiles, exportWayAttributeMapsToFiles } from "./attributeMaps.js";
 
 const inputName = "kralovehradecky-latest";
 const doDump = false;
@@ -20,11 +17,9 @@ void doIt();
 
 async function doIt() {
   const startTime = Date.now();
-  const dump = doDump
-    ? fs.createWriteStream(`output/${inputName}-dump.txt`)
-    : null;
+  const dump = doDump ? fs.createWriteStream(`output/${inputName}-dump.txt`) : null;
   const collector = new Data();
-  for await (const it of createOSMStream(`input/${inputName}.osm.pbf`)) {
+  for await (const it of createOSMStream(`../input/${inputName}.osm.pbf`)) {
     const item = it as OSMItem;
     dump?.write(JSON.stringify(item) + "\n");
     collector.visit(item);
